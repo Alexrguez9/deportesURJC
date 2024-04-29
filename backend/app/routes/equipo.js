6b554d43b2205b50e304c7bf1cb2fc6b2dd9a46a
Alexrguez9 <72986293+Alexrguez9@users.noreply.github.com>
2024-04-29 14:18:03 +0200

    Created CRUD for equipos, equipo-controller, equipo-model and equipo-routes

---
+++ backend/app/routes/equipo.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const controller = require('../controllers/equipo')
+
+const router = express.Router()
+const path = 'equipos'
+
+// Ruta GET equipos
+router.get(`/${path}`, controller.getData);
+
+// Ruta POST equipo
+router.post(`/${path}`, controller.insertData);
+
+// Ruta PUT equipo
+router.put(`/${path}/:id`, controller.updateOne);
+
+// Ruta DELETE equipo
+router.delete(`/${path}/:id`, controller.deleteOne);
+
+module.exports = router
