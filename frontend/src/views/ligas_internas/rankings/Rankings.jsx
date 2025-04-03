e8e3685a52a179dd604029d85eae8998df247ac2
Alexrguez9 <72986293+Alexrguez9@users.noreply.github.com>
2025-04-03 23:46:59 +0200

    Not show all teams in rankings by default

--- frontend/src/views/ligas_internas/rankings/Rankings.jsx
+++ frontend/src/views/ligas_internas/rankings/Rankings.jsx
@@ -5,7 +5,7 @@ import { useTeamsAndResults } from "../../../context/TeamsAndResultsContext";
 
 const Rankings = () => {
     const { teams, fetchTeams } = useTeamsAndResults();
-    const [selectedSport, setSelectedSport] = useState('Todos');
+    const [selectedSport, setSelectedSport] = useState('');
 
     useEffect(() => {
         fetchTeams();
@@ -15,9 +15,9 @@ const Rankings = () => {
         setSelectedSport(event.target.value);
     };
 
-    const filteredTeams = teams
-        .filter((team) => selectedSport === 'Todos' || team.sport === selectedSport)
-        .sort((a, b) => b.points - a.points); // Sort by points in descending order
+    const filteredTeams = selectedSport
+        ? teams.filter((team) => team.sport === selectedSport).sort((a, b) => b.points - a.points)
+        : []; // Muestra equipos solo si se ha seleccionado un deporte
 
     return (
         <div id="component-content" className="content">
@@ -28,41 +28,44 @@ const Rankings = () => {
                 <h1>Clasificaciones Ligas Internas</h1>
                 <p>Consulta las clasificaciones de las ligas internas de la URJC</p>
             </div>
-            <select value={selectedSport} onChange={handleDeporteChange}>
-                <option value="Todos">Todos</option>
+            <select value={selectedSport} onChange={handleDeporteChange} className="select-sport">
+                <option value="">Elige un deporte</option>
                 <option value="Fútbol-7">Fútbol-7</option>
                 <option value="Fútbol-sala">Fútbol-sala</option>
                 <option value="Baloncesto">Baloncesto</option>
                 <option value="Voleibol">Voleibol</option>
             </select>
 
-            {filteredTeams?.length === 0 ?
-                    <p>No hay encuentros de {selectedSport} para mostrar</p> :
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Deporte</th>
-                        <th>Ganados</th>
-                        <th>Empatados</th>
-                        <th>Perdidos</th>
-                        <th>Puntos</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredTeams.map((team) => (
-                        <tr key={team._id}>
-                            <td>{team.name}</td>
-                            <td>{team.sport}</td>
-                            <td>{team.results.wins}</td>
-                            <td>{team.results.draws}</td>
-                            <td>{team.results.losses}</td>
-                            <td>{team.points}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            }
+            {selectedSport && (
+                filteredTeams?.length === 0 ? (
+                    <p>No hay encuentros de {selectedSport} para mostrar</p>
+                ) : (
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Deporte</th>
+                                <th>Ganados</th>
+                                <th>Empatados</th>
+                                <th>Perdidos</th>
+                                <th>Puntos</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {filteredTeams.map((team) => (
+                                <tr key={team._id}>
+                                    <td>{team.name}</td>
+                                    <td>{team.sport}</td>
+                                    <td>{team.results.wins}</td>
+                                    <td>{team.results.draws}</td>
+                                    <td>{team.results.losses}</td>
+                                    <td>{team.points}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )
+            )}
         </div>
     );
 };
