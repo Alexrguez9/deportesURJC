41f62faf04716c26267ed63d0405d4028a0d67f9
Alexrguez9 <72986293+Alexrguez9@users.noreply.github.com>
2025-04-04 18:08:07 +0200

    Fix case-sensitive path: use internLeaguesContent correctly

---
+++ frontend/src/components/InternLeaguesContent/Temp.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import "./InternLeaguesContent.css";
+import { MdEmojiEvents } from "react-icons/md";
+import { PiRankingLight } from "react-icons/pi";
+
+const MainLigasInternas = () => {
+    return (
+        <>
+            <h1>Ligas Internas</h1>
+            <p>Bienvenido a la página de Ligas Internas de URJC Deportes. Aquí podrás consultar los encuentros y clasificaciones de las Ligas Internas.</p>
+            <section>
+                <div className='ligas-internas'>
+                    <Link to="/ligas-internas/encuentros">
+                        <div className='horizontal-card'>
+                            <MdEmojiEvents style={{ padding: '1rem', height: '2rem', width: '2rem'  }}/>
+                            <p>Encuentros</p>
+                        </div>
+                    </Link>
+                    <Link to="/ligas-internas/clasificaciones">
+                        <div className='horizontal-card'>
+                            <PiRankingLight style={{ padding: '1rem', height: '2rem', width: '2rem'  }}/>
+                            <p>Clasificaciones</p>
+                        </div>
+                    </Link>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default MainLigasInternas;
